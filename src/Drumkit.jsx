2601160b58fa5a09ba4a09945bea4a96a2c3a854
lasernoy <cloudy.nny07@gmail.com>
2024-02-19 10:47:13 +0800

    mouse click on event delegation

--- src/Drumkit.jsx
+++ src/Drumkit.jsx
@@ -27,10 +27,14 @@ export default function Drumkit({ kit }) {
 
     // Callback: on 'mousedown' user-click event.
     const onDrumClick = (event) => {
-        window.console.log('\tmousedown:', event.target, event.target.lastElementChild);
-        const drumPad = event.target.lastElementChild;
-        // drumPad.currentTime = 0;
-        drumPad.load();
+        window.console.log('onDrumClick\n\tEvent:', event, event.target, 'target-id:', event.target.id);
+        const kitObj = kit.filter(obj => obj['id'] === event.target.id);
+        window.console.log('\tkit-object:', kitObj);
+        const kitObjIndex = kit.indexOf(kitObj[0]);
+        window.console.log('kit-object-index:', kitObjIndex);
+        const drumPad = document.getElementById(kit[kitObjIndex]['key']);
+        window.console.log('\tdrum-pad:', drumPad);
+        drumPad.currentTime = 0;
         drumPad.play();
     };
 
