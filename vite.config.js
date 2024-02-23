16a0380b487f9c6903dd0aa4ff01e10c8643c535
lasernoy <cloudy.nny07@gmail.com>
2024-02-23 12:34:15 +0800

    fcc push

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/fcc-drum-machine-1.0/"
+  base: "/fcc-drum-machine-1.0/"
 })
